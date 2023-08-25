ab7b424c7eb5fbe80da659b0c100c9fff87af2f4
Canon <canon@newsbreak.com@Canons-MacBook-Pro.local>
2023-08-25 17:55:20 +0800

    up data

---
+++ sh_a100/inf_peft.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+DATA_DIR="/mnt/nlp/xingguang/llama/datasets/nb_training/clickbait"
+DATA_DIR="/mnt/nlp/xingguang/llama/datasets/nb_training/grammar_c4200m_seq2seq"
+#DATA_DIR="/mnt/nlp/xingguang/llama/datasets/nb_training/grammar_c4200m_single"
+DATA_DIR="/home/paperspace/datasets/grammar_c4200m_seq2seq"
+
+
+WORK_DIR="/home/paperspace/llama/ckpt.peft"
+MODEL_NAME="meta-llama/Llama-2-7b-hf"
+DATASET_NAME="my_allin_one_dataset"
+TAG="grammar-seq2seq"
+#TAG="grammar-single"
+
+CUDA_VISIBLE_DEVICES="0,1" python llama_inf.py \
+  --length_penalty 3 \
+  --model_name ${MODEL_NAME} \
+  --peft_model ${WORK_DIR}/${MODEL_NAME}/${DATASET_NAME}/${TAG}-peft/epoch_004 \
+  --dataset my_allin_one_dataset \
+  --input_file ${DATA_DIR}/valid.txt \
+  --output_file ${DATA_DIR}/valid.txt.pred
