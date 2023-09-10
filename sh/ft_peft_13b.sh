330bf7d1bd789f15641053128b1c7cf9c0fd15bc
Canon <canon@newsbreak.com>
2023-09-10 22:39:16 +0800

    update train & inf scripts

---
+++ sh/ft_peft_13b.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+
+WORK_DIR="/home/paperspace/xingguang/llama/ckpt.peft"
+MODEL_NAME="meta-llama/Llama-2-13b-hf"
+DATASET_NAME="my_allin_one_dataset"
+TAG="answer_extractor.v011.13b.2e-5"
+ts=$(date +"%Y-%m-%d")
+
+cd ..
+
+CUDA_VISIBLE_DEVICES="4,5,6,7" torchrun \
+  --nnodes 1 \
+  --nproc_per_node 4 \
+  ./llama_finetuning.py \
+  --enable_fsdp  \
+  --use_peft \
+  --peft_method lora \
+  --model_name ${MODEL_NAME} \
+  --dataset ${DATASET_NAME} \
+  --save_model \
+  --pure_bf16 \
+  --output_dir ${WORK_DIR}/${MODEL_NAME}/${DATASET_NAME}/${TAG}-peft/ \
+  --lr 0.00002 \
+  --val_batch_size 8 \
+  --batch_size_training 8 \
+  --micro_batch_size 8 \
+  --num_epochs 10 \
+  --evaluation_steps 50 \
+  --check_point_steps 1000 \
+  --wandb_name ${MODEL_NAME}-${DATASET_NAME}-${TAG}-${ts}
+
+cd ../
+
---
+++ sh/inf_peft_13b.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+
+WORK_DIR="/home/paperspace/xingguang/llama/ckpt.peft"
+MODEL_NAME="meta-llama/Llama-2-13b-hf"
+DATASET_NAME="my_allin_one_dataset"
+TAG="answer_extractor.v009.13b.2e-5"
+INPUT_FILE="answer_extractor.v009/valid.txt"
+
+
+cd ..
+
+CUDA_VISIBLE_DEVICES="2,3" python llama_inf.py \
+  --length_penalty 1 \
+	--num_beams 10 \
+	--do_sample 0 \
+  --model_name ${MODEL_NAME} \
+  --peft_model ${WORK_DIR}/${MODEL_NAME}/${DATASET_NAME}/${TAG}-peft/best_model \
+  --dataset my_allin_one_dataset \
+  --input_file ${INPUT_FILE}
