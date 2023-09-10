8e2610842fd61048b041ab436e6ff8cd9aa89fda
Canon <canon@newsbreak.com>
2023-09-10 15:28:22 +0800

    up inference scripts

--- sh/inf_peft.sh
+++ sh/inf_peft.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -x
 
-#INPUT_FILE="clickbait/valid.txt"
-#INPUT_FILE="grammar_c4200m_single/valid.txt"
-#INPUT_FILE="grammar_c4200m_seq2seq/valid.txt"
-INPUT_FILE="answer_extractor.v007/valid.txt"
 
 WORK_DIR="/home/paperspace/xingguang/llama/ckpt.peft"
 MODEL_NAME="meta-llama/Llama-2-7b-hf"
 DATASET_NAME="my_allin_one_dataset"
-#TAG="grammar-seq2seq"
-#TAG="grammar-single"
-TAG="answer_extractor.v007"
+TAG="answer_extractor.v009.2e-5"
+INPUT_FILE="answer_extractor.v009/valid.txt"
+
 
 cd ..
 
-CUDA_VISIBLE_DEVICES="0,1" python llama_inf.py \
-  --length_penalty 1 \
+CUDA_VISIBLE_DEVICES="2,3" python llama_inf.py \
+  --length_penalty 0.5 \
+	--num_beams 10 \
+	--do_sample 0 \
   --model_name ${MODEL_NAME} \
   --peft_model ${WORK_DIR}/${MODEL_NAME}/${DATASET_NAME}/${TAG}-peft/best_model \
   --dataset my_allin_one_dataset \
--- sh/start_svr_v008.sh
+++ sh/start_svr_v008.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python llama_svr.py \
   --port 1201 \
 	--length_penalty 0.5 \
-	--num_beams 5 \
+	--num_beams 10 \
 	--do_sample 0 \
 	--model_name meta-llama/Llama-2-7b-hf \
 	--peft_model /mnt/nlp/xingguang/llama/answer_extractor.v008.2e-5-peft/best_model
