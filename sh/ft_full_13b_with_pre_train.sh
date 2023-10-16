029cd2c32dcc970af5d3498e84aaa423ae56ad04
Canon <canon@newsbreak.com>
2023-10-16 10:44:12 +0800

    update script

---
+++ sh/ft_full_13b_with_pre_train.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -x
+
+MODEL_TYPE="13b"
+WORK_DIR="/home/paperspace/xingguang/llama/ckpt.full/${MODEL_TYPE}"
+MODEL_NAME="meta-llama/Llama-2-${MODEL_TYPE}-hf"
+DATASET_NAME="my_allin_one_dataset"
+DATASET_TYPE=""
+DATASET_SUB_DIR="answer_extractor.v024"
+PRE_TRAIN_MODEL="${WORK_DIR}/pre-training-ariticle/tokenized.13b-13b.3e-5.full.B4.E1/epoch_000.hf"
+
+LR=3e-5
+BATCH_SIZE=4
+EPOCH=2
+
+TAG="${MODEL_TYPE}.${LR}.full.B${BATCH_SIZE}.E${EPOCH}.withPreTrain"
+ts=$(date +"%Y-%m-%d")
+
+cd ..
+
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" torchrun \
+  --nnodes 1 \
+  --nproc_per_node 8 \
+  --master_port=1201 \
+  ./llama_finetuning.py \
+  --enable_fsdp  \
+  --model_name "${PRE_TRAIN_MODEL}" \
+  --dataset "${DATASET_NAME}" \
+  --dataset_tag "${DATASET_TYPE}" \
+  --dataset_sub_dir_prefix "${DATASET_SUB_DIR}" \
+  --save_model \
+  --pure_bf16 \
+  --output_dir "${WORK_DIR}/${DATASET_SUB_DIR}-${TAG}"/ \
+  --lr ${LR} \
+  --val_batch_size ${BATCH_SIZE} \
+  --batch_size_training ${BATCH_SIZE} \
+  --micro_batch_size 4 \
+  --num_epochs ${EPOCH} \
+  --evaluation_steps 100 \
+  --check_point_steps 2000 \
+  --wandb_name ${MODEL_NAME}-${DATASET_NAME}-${DATASET_SUB_DIR}-${TAG}-${ts}
+
+cd ../
+
+python inference/checkpoint_converter_fsdp_hf.py \
+  --fsdp_checkpoint_path ${WORK_DIR}/${DATASET_NAME}/${TAG}-${MODEL_NAME}/ \
+  --consolidated_model_path ${WORK_DIR}/${DATASET_NAME}/${TAG}-hf/ \
+  --HF_model_path_or_name ${MODEL_NAME}
